dd8999877a7ae8301179357b4f27b174aa9f0e55
zhaotao1987 <mlwbglorious@gmail.com>
2018-07-05 20:11:55 +0200

    synteny network clustering--using infomap "igraph"

---
+++ infomap.r
@@ -0,0 +1,42 @@
+#!/usr/bin/Rscript
+# Usage: Rscript myscript.R networkfile(in 2 columns) outputname 
+#
+# The network file should be separated by tab
+
+
+# Try if args work.
+#head(args[1])
+
+require(igraph)
+args<-commandArgs(TRUE)
+#data <-read.table(args[1],sep='\t',header=F,skip=4)
+data <-read.table(args[1],sep=' ',header=F)
+network <- graph.data.frame(data,directed=F)
+
+# net_simple <- simplify(network)
+# Write_graph(net_simple,file="simple_network",format=c("ncol"))
+# write_graph(net_simple,file=args[2],format=c("ncol"))
+
+# Kcore Table
+# kc <- coreness(net_simple,mode="all")
+# write.table(kc,args[4],quote=F,sep='\t',row.names=T,col.names=F)
+# Now you want to nodes with a kcore higher than 3
+
+# Now we do clustering for the network
+# clusters1 <- cluster_louvain(network)
+# clusters2 <- cluster_walktrap(network)
+clusters3 <- cluster_infomap(network)
+out3 <- as.data.frame(list(names=clusters3$names, mem=clusters3$membership))
+write.table(out3,args[2],quote=F,sep='\t',row.names=F)
+
+# out1 <- as.data.frame(list(names=clusters1$names, mem=clusters1$membership))
+# write.table(out1,args[2],quote=F,sep='\t',row.names=F)
+
+# out2 <- as.data.frame(list(names=clusters2$names, mem=clusters2$membership))
+# write.table(out2,args[3],quote=F,sep='\t',row.names=F)
+
+
+
+
+
+
