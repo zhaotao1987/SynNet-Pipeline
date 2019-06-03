986bdcad76d25eebacce3e203bd12ce64a0cddf6
Tao <mlwbglorious@gmail.com>
2019-06-03 19:03:31 +0200

    Update Phylogenomic_Profiling.r

--- Phylogenomic_Profiling.r
+++ Phylogenomic_Profiling.r
@@ -1,25 +1,18 @@
 library(pheatmap)
 library(vegan)
+
 args<-commandArgs(TRUE)
-
-#data <- read.table(args[1],sep='\t',header=T) # By default
-
-# Warning messages:
-# 1: In scan(file = file, what = what, sep = sep, quote = quote, dec = dec,  :
-  # EOF within quoted string
-# 2: In scan(file = file, what = what, sep = sep, quote = quote, dec = dec,  :
-  # number of items read is not a multiple of the number of columns
 
 # pre-treatment
   
 data <- read.table(args[1],header=T)  # Sometimes You have to use this one! For problems above ! I dont  know why
+data$species <- substr(data$names,1,3) # extract first three letters of the gene name as species id
+colnames(data) <-c("Gene","Cluster","Species")
+data <- data[,c(1,3,2)]
 	# Input
 	# Gene	Species	Cluster
 	# AlyrAL1G19310	Aly	13296
 	# AlyrAL1G19350	Aly	75
-data$species <- substr(data$names,1,3) # extract first three letters of the gene name as species id
-colnames(data) <-c("Gene","Cluster","Species")
-data <- data[,c(1,3,2)]
 
 # cluster by rows, Species by columns
 out <- table(data$Cluster,data$Species) 
@@ -44,17 +37,17 @@ new3 <-new2[-1,]
 #out <- out[,myorder]
 
 write.table(new3,args[2],col.names =NA,quote=F) # export output
-	# Output sample
+	
+# Output sample
+
 	# Aly ath atr can csi dca Ebr hel Lsa osa oth sly Tar TKS vvi
 	# 1 28 28 0 65 53 43 0 59 67 25 17 59 112 9 84
 	# 2 7 3 11 175 17 9 0 5 19 44 1 28 100 6 13
 	# 3 36 62 15 16 18 12 0 39 22 36 5 14 48 3 32
 
-#require(igraph)
-
 matrixp <-data.matrix(new3)
 breaksList <- c(0,0.9,1.58,1.9,10) # Set range # 
-#mycolor <- c("white","#E8E8E8","#909090","#303030") # Grey Scale # 设定对应的颜色
+#mycolor <- c("white","#E8E8E8","#909090","#303030") # Grey Scale
 mycolor <- c("white","#8FB0D7","#FDC87A","#F70C0E") # Red- Orange- Blue Scale #
 
 d <- vegdist(log2(matrixp+1),method="jaccard") 
